c7488f1bfee8c1b19a46ae2cfd5dc8f7a1b9cc5d
Ben Simondet <Ben Simondet>
2016-06-16 11:39:59 -0500

    fixed animations

--- client/app/happyFunTime/happyFunTime.js
+++ client/app/happyFunTime/happyFunTime.js
@@ -1,9 +1,9 @@
-'use strict';
-
-angular.module('beckmenProjectApp')
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/happyFunTime', {
-        template: '<happy-fun-time></happy-fun-time>'
-      });
-  });
+// 'use strict';
+//
+// angular.module('beckmenProjectApp')
+//   .config(function ($routeProvider) {
+//     $routeProvider
+//       .when('/happyFunTime', {
+//         template: '<happy-fun-time></happy-fun-time>'
+//       });
+//   });
