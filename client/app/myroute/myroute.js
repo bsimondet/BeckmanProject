f25593d8299a4b673a94931e52d32a170f701c08
Ethan <ethan@myDebianSys>
2016-06-16 09:26:40 -0500

    Deleted the myroute page

--- client/app/myroute/myroute.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('alienAttack')
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/myroute', {
-        template: '<myroute></myroute>'
-      });
-  });
