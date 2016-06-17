528c9e30646c74dbca81b93817683b2bf17b6a90
Ethan <ethan@myDebianSys>
2016-06-16 22:17:25 -0500

    Bug fixes

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -3,16 +3,23 @@
 class SettingsController {
 
   constructor(Auth) {
+    // this.user = {};
+    this.errors = {};
+    this.submitted = false;
+
     this.Auth = Auth;
+    this.isLoggedIn = Auth.isLoggedIn;
+    // this.$location = $location;
   }
 
   changePassword(form) {
     this.submitted = true;
-
     if (form.$valid) {
       this.Auth.changePassword(this.user.oldPassword, this.user.newPassword)
         .then(() => {
           this.message = 'Password successfully changed.';
+          this.$location.path('/login');
+          // window.location.href = '/home';
         })
         .catch(() => {
           form.password.$setValidity('mongoose', false);
